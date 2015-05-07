7b1fe39eb9bfa98589f9652a2d5f89191b338f2c
kilisoria <cristiansoria@gmail.com>
2015-05-07 15:54:16 -0300

    Task 03 - Updated code for the users section. Removed unnecessary ones.

--- Source/Hiperion/Hiperion/Scripts/app/app.js
+++ Source/Hiperion/Hiperion/Scripts/app/app.js
@@ -1,4 +1,13 @@
 ﻿'use strict';
 
-angular.module('hiperionApp', ['ngTable', 'ngTableExport','ngDialog'])
+angular.module('hiperionApp', ['ngTable', 'ngTableExport', 'ngDialog'])
+.filter('userRange', function () {
+    return function (input, min, max) {
+        min = parseInt(min); //Make string input int
+        max = parseInt(max);
+        for (var i = min; i < max; i++)
+            input.push(i);
+        return input;
+    };
+});
 
