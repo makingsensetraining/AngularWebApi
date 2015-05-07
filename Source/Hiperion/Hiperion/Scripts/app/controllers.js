91ccd5428ce0fb185d10f88fc202eb5525cd048f
kilisoria <cristiansoria@gmail.com>
2015-05-07 10:43:10 -0300

    Task02 - Updated code and added more features for the ngDialog.

--- Source/Hiperion/Hiperion/Scripts/app/controllers.js
+++ Source/Hiperion/Hiperion/Scripts/app/controllers.js
@@ -8,12 +8,35 @@ angular.module('hiperionApp')
             $scope.activeSection = section;
             $scope.title = section.toUpperCase();
         };
+        $scope.name = "";
+        $scope.comment = "";
+        $scope.comments = [];
+        $scope.isCommentsVisible = false;
         $scope.showStatistics = function () {
+            $scope.isCommentsVisible = false;
             ngDialog.open({
                 template: 'stadisticsTemplate',
                 className: 'ngdialog-theme-default'
             });
-        }
+        };
+        $scope.showComments = function () {
+            $scope.isCommentsVisible = true;
+        };
+        $scope.preCloseCallbackOnScope = function (value) {
+            return true;
+        };
+        $scope.addComment = function () {
+            ngDialog.openConfirm({
+                template: 'commentTemplate',
+                className: 'ngdialog-theme-default',
+                preCloseCallback: 'preCloseCallbackOnScope',
+                scope: $scope
+            }).then(function (data) {
+                $scope.comments.push(data);
+            }, function (reason) {
+                console.log('Modal promise rejected. Reason: ', reason);
+            });
+        };
     })
     .controller('UserCtrl', function ($scope, $filter, ngTableParams, $sce) {
         $scope.tableForms = ['None', 'Default', 'Sort', 'Filter', 'Styling-ExportCsv'];
