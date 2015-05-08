a304ae0e3f8762e03927c204a1055fbb5c2a757f
Daniel Altamirano <daniel.altamirano@glafon.com>
2015-05-08 15:23:23 -0300

    User Controller

---
+++ Source/Hiperion/Hiperion/Services/UserServices.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hiperion.Services
+{
+    using AutoMapper;
+    using Domain;
+    using Interfaces;
+    using Models;
+    using Repositories.Interfaces;
+
+    public class UserServices : IUserServices
+    {
+        private readonly IUserRepository _repository;
+
+        public UserServices(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<UserDto> GetAllUsers()
+        {
+            var users = _repository.GetAllValues();
+
+            var userDtos = Mapper.Map<IList<User>, IList<UserDto>>(users);
+
+            return userDtos;
+        }
+
+        public bool SaveOrUpdateUser(UserDto userDto)
+        {
+            var user = Mapper.Map<UserDto, User>(userDto);
+
+            //add some bussines logic before update DB
+
+            _repository.SaveOrUpdateUser(user);
+
+            return true;
+        }
+
+        public void DeleteUser(int id)
+        {
+            //add some bussines logic before update DB
+
+            _repository.DeleteUser(id);
+        }
+    }
+}
