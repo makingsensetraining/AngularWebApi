66b181e83b0461ed01add369837ded0426ed6a0e
kilisoria <cristiansoria@gmail.com>
2015-05-11 14:44:25 -0300

    Task 05 - Updated tests.

--- Source/Hiperion/Hiperion/Test/test.js
+++ Source/Hiperion/Hiperion/Test/test.js
@@ -1,10 +1,8 @@
 'use strict';
 
-describe("Test 001 - Simple one", function () { it("OK", function () { expect(true).toBe(true); }); });
+describe("Test 001 - Simple one", function () { it("This is a simple expect to test the conexion", function () { expect(true).toBe(true); }); });
 
-describe("Test 002 - Simple one", function () { it("Ok", function () { expect(true).toBe(true); }); });
-
-describe("Test 003 - User Controller", function () {
+describe("Test 002 - User Controller", function () {
 
     beforeEach(module('hiperionApp'));
 
@@ -17,20 +15,20 @@ describe("Test 003 - User Controller", function () {
         });
     }));
 
-    it('test 003.1', function () {
+    it('test 002.1', function () {
         expect(scope.name).toBe('');
     });
 
-    it('test 003.2', function () {
+    it('test 002.2', function () {
         expect(scope.lastName).toBe('');
     });
 
-    it('test 003.3', function () {
+    it('test 002.3', function () {
         expect(scope.tableParams.count()).toBe(5);
     });
 });
 
-describe("Test 004 - Home Controller", function () {
+describe("Test 003 - Home Controller", function () {
     beforeEach(module('hiperionApp'));
 
     var controller, scope;
@@ -42,15 +40,32 @@ describe("Test 004 - Home Controller", function () {
         });
     }));
 
-    it('test 004.1', function () {
+    it('test 003.1', function () {
         expect(scope.title).toBe('HOME');
     });
 
-    it('test 004.2', function () {
+    it('test 003.2', function () {
         expect(scope.addComment()).not.toBe(null);
     });
 
-    it('test 004.3', function () {
+    it('test 003.3', function () {
         expect(scope.title).toMatch('H');
+    });
+});
+
+describe("Test 004 - About Controller", function () {
+    beforeEach(module('hiperionApp'));
+
+    var controller, scope;
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        controller = $controller('AboutCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('test 004.1', function () {
+        expect(scope.description).not.toBe('');
     });
 });
