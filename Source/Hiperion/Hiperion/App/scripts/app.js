0c94d456235f3e6649b1f49a32c5437e92bfe17b
Daniel Altamirano <ndaltamirano@gmail.com>
2015-05-12 11:59:28 -0300

    Edit/Save/Cancel functions for users

--- Source/Hiperion/Hiperion/App/scripts/app.js
+++ Source/Hiperion/Hiperion/App/scripts/app.js
@@ -8,13 +8,4 @@ angular.module('hiperionApp', ['ngRoute', 'ngTable', 'ngTableExport', 'ngDialog'
             .when('/about', { templateUrl: 'App/views/about.html', controller: 'AboutCtrl' })
             .otherwise({ redirectTo: '/home' });
 
-    })
-    .filter('userRange', function() {
-        return function(input, min, max) {
-            min = parseInt(min);
-            max = parseInt(max);
-            for (var i = min; i < max; i++)
-                input.push(i);
-            return input;
-        };
     });
--- Source/Hiperion/Hiperion/App/scripts/controllers/user.js
+++ Source/Hiperion/Hiperion/App/scripts/controllers/user.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('hiperionApp')
-    .controller('UserCtrl', function ($scope, $http, $filter, ngTableParams, $sce, ngDialog) {
+    .controller('UserCtrl', function($scope, $http, $filter, ngTableParams, $sce, ngDialog) {
         //// TODO: Replace this with the data from the web Api.
-        
+
         //var data = [{ id: 1,name: 'Nick', lastName: 'Rimando', age: 26 },
         //                { id: 2, name: 'William', lastName: 'Yarbrough', age: 26 },
         //                { id: 3, name: 'DeAndre', lastName: 'Yedlin', age: 60 },
@@ -19,15 +19,15 @@ angular.module('hiperionApp')
 
         var data = [];
         $http.get('/api/User').
-            success(function (result, status, headers, config) {
+            success(function(result, status, headers, config) {
                 data = result;
                 $scope.tableParams.reload();
             }).
-            error(function (result, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            }
-        );
+            error(function(result, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                }
+            );
 
         $scope.tableParams = new ngTableParams({
             page: 1,
@@ -36,14 +36,14 @@ angular.module('hiperionApp')
             filter: { name: '', lastName: '' }
         }, {
             total: data.length,
-            getData: function ($defer, params) {
+            getData: function($defer, params) {
                 var orderedData = params.filter() ?
-                                   $filter('filter')(data, params.filter()) :
-                                   data;
+                    $filter('filter')(data, params.filter()) :
+                    data;
 
                 orderedData = params.sorting() ?
-                                    $filter('orderBy')(orderedData, params.orderBy()) :
-                                    orderedData;
+                    $filter('orderBy')(orderedData, params.orderBy()) :
+                    orderedData;
 
                 $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
             }
@@ -54,21 +54,41 @@ angular.module('hiperionApp')
         $scope.lastName = "";
         $scope.age = "";
 
-        $scope.addNewUser = function () {
+        $scope.addNewUser = function() {
             ngDialog.openConfirm({
                 template: 'App/views/userDialog.html',
                 className: 'ngdialog-theme-default',
                 preCloseCallback: 'preCloseCallbackOnScope',
                 scope: $scope
-            }).then(function (user) {
+            }).then(function(user) {
                 data.push(user);
-            }, function (reason) {
+            }, function(reason) {
             });
         };
 
-        $scope.removeUser = function (user) {
-            data = _.filter(data, function (item) {
+        $scope.editUser = function (user) {
+            user.$edit = true;
+        };
+
+        $scope.saveUser = function (user) {
+            user.$edit = false;
+        };
+
+        $scope.cancelEditUser = function (user) {
+            user.$edit = false;
+        };
+
+        $scope.removeUser = function(user) {
+            data = _.filter(data, function(item) {
                 return item.id != user.id;
             });
+        };
+    }).filter('userRange', function () {
+        return function (input, min, max) {
+            min = parseInt(min);
+            max = parseInt(max);
+            for (var i = min; i < max; i++)
+                input.push(i);
+            return input;
         };
     });
