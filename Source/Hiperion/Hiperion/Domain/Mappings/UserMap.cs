22bdf1720a73db66ee37f880d6cbf4d7b204c647
Daniel Altamirano <ndaltamirano@gmail.com>
2015-05-14 22:22:47 -0300

    UserMap and RoleMap classes

---
+++ Source/Hiperion/Hiperion/Domain/Mappings/UserMap.cs
@@ -0,0 +1,19 @@
+﻿namespace Hiperion.Domain.Mappings
+{
+    using System.Data.Entity.ModelConfiguration;
+
+    public class UserMap : EntityTypeConfiguration<User>
+    {
+        public UserMap()
+        {
+            // Primary Key
+            HasKey(role => role.Id);
+
+            //Properties
+            Property(t => t.Id).HasColumnName("Id");
+            Property(t => t.FirstName).HasColumnName("FirstName");
+            Property(t => t.LastName).HasColumnName("LastName");
+            Property(t => t.Age).HasColumnName("Age");
+        }
+    }
+}
