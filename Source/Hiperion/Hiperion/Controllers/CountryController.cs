f1b613013fe37bc7aaa135a5ec93673fd55d0403
Daniel Altamirano <ndaltamirano@gmail.com>
2015-05-19 10:17:23 -0300

    Country Controller

---
+++ Source/Hiperion/Hiperion/Controllers/CountryController.cs
@@ -0,0 +1,46 @@
+﻿namespace Hiperion.Controllers
+{
+    #region References
+
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using Models;
+    using Services.Interfaces;
+
+    #endregion
+
+    public class CountryController : ApiController
+    {
+        private readonly ICountryServices _countryServices;
+
+        public CountryController(ICountryServices countryServices)
+        {
+            _countryServices = countryServices;
+        }
+
+        //GET: /api/Country
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var countries = _countryServices.GetAllCountries();
+            return countries == null
+                ? Request.CreateResponse(HttpStatusCode.NotFound)
+                : Request.CreateResponse(HttpStatusCode.OK, countries);
+        }
+
+        [HttpPost] // POST api/Country
+        public HttpResponseMessage Post(CountryDto country)
+        {
+            _countryServices.SaveOrUpdateCountry(country);
+            return Request.CreateResponse(HttpStatusCode.OK, country);
+        }
+
+        [HttpDelete] // DELETE api/Country/5
+        public HttpResponseMessage Delete(int id)
+        {
+            _countryServices.DeleteCountry(id);
+            return Request.CreateResponse(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ Source/Hiperion/Hiperion/Services/CountryServices.cs
@@ -0,0 +1,52 @@
+﻿namespace Hiperion.Services
+{
+    #region References
+
+    using System.Collections.Generic;
+    using AutoMapper;
+    using Domain;
+    using Interfaces;
+    using Models;
+    using Repositories.Interfaces;
+
+    #endregion
+
+    public class CountryServices : ICountryServices
+    {
+        private readonly IMappingEngine _mapperEngine;
+        private readonly ICountryRepository _repository;
+
+        public CountryServices(ICountryRepository repository, IMappingEngine mapperEngine)
+        {
+            _repository = repository;
+            _mapperEngine = mapperEngine;
+        }
+
+        public IEnumerable<CountryDto> GetAllCountries()
+        {
+            var countries = _repository.GetAllValues();
+
+            var countryDtos = _mapperEngine.Map<IList<Country>, IList<CountryDto>>(countries);
+
+            return countryDtos;
+        }
+
+        public bool SaveOrUpdateCountry(CountryDto countryDto)
+        {
+            var country = _mapperEngine.Map<CountryDto, Country>(countryDto);
+
+            //add some bussines logic before update DB
+
+            _repository.SaveOrUpdateCountry(country);
+
+            return true;
+        }
+
+        public void DeleteCountry(int id)
+        {
+            //add some bussines logic before update DB
+
+            _repository.DeleteCountry(id);
+        }
+    }
+}
